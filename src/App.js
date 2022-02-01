76e72c3493ba8c7225532321215e7402824d5c83
chei7tain <fredrickokwuobi@gmail.com>
2022-02-01 21:47:14 +0100

    imported the created components to the App components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
+import { Ballance } from "./components/Ballance/Ballance";
+import { Header } from "./components/Header/Header";
+import { IncomeExpense } from "./components/IncomeExpense/IncomeExpense";
+import { TransactionList } from "./components/TransactionList/TransactionList";
 
-const App = () => "Hello world";
+const App = () => (
+  <>
+    <Header />
+    <Ballance />
+    <IncomeExpense />
+    <TransactionList/>
+  </>
+);
 
 export default App;
